fc549a31b17b6fb67c7719b57975ae956b2b612f
Abdulaziz Ali <rediabdulaziz@gmail.com>
2020-08-27 23:47:41 +0300

    add authuntication and guards

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :posts
+  resources :posts, only: %i[index new edit create update show]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
