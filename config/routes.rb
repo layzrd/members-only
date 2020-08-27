ea0b479651502f9b59fad7d47217ed45f2c84b58
Abdulaziz Ali <rediabdulaziz@gmail.com>
2020-08-28 00:09:03 +0300

    fix delete functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
-  resources :posts, only: %i[index new edit create update show]
+  resources :posts, only: %i[index new edit create update show destroy]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
